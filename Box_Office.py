d55f38e173fe4ceeabc90cc079227115d7aaac46
Jason Zhong <jasonzhong0408@gmail.com>
2022-09-27 17:49:21 -0400

    read in data for box office

--- Box_Office.py
+++ Box_Office.py
@@ -2,17 +2,11 @@ import csv
 import random
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_selection import RFE
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
-from sklearn.feature_selection import mutual_info_classif
+
 from matplotlib import pyplot
 
 #Code to create a new csv file with the features I want
-
+"""
 f = open('Netflix Preprocessed2.csv', 'w', newline='')
 dataset = 'Netflix Dataset Latest 2021.csv'
 
@@ -26,3 +20,30 @@ writer.writerow(myRow)
 
 for line in csvreader:
     writer.writerow([line[17]] + line[1:17] + line[18:21] + [line[24]])
+
+"""
+
+dataset = 'Netflix Preprocessed2.csv'
+csvreader = csv.reader(open(dataset))
+header = next(csvreader)
+
+#x is an array of all the features used to predict Box Office
+#y is an array of the Box Office
+x = []
+Y = []
+for line in csvreader:
+    if line[0] != '':
+        x.append(line[1:])
+        firstIntFound = False
+        s = ''
+        for c in line[0]:
+            if c.isdigit():
+                if firstIntFound:
+                    s+='0'
+                else:
+                    s+= c
+                    firstIntFound = True
+        Y.append(int(s))
+print(len(x))
+print(len(Y))
+print(Y[0])
